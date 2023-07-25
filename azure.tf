f16f2514511de80fba138e4bec919cd0e3019122
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-07-25 16:24:58 +0530

    Create azure.tf

---
+++ azure.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = ""
+   client_id = ""
+   client_secret = ""
+   tenant_id  = ""
+}
+
+
+resource "azurerm_resource_group" "main" {
+  name     = "azvm"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "azvm_main" {
+  name                = "aznetwork"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.azvm_main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "azni_main" {
+  name                = "nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "azvm_main" {
+  name                            = "alvm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = "eastus"
+  size                            = "Standard_D2s_v3"
+  admin_username                  = ""
+  admin_password                  = ""
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.azni_main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
