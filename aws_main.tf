f213cf42c48e696e2796ecf10f9f95869a1be07d
BhaskarRao-D <128477363+BhaskarRao-D@users.noreply.github.com>
2023-08-14 15:03:19 +0530

    Update aws_main.tf

--- aws_main.tf
+++ aws_main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "aws_ec2" {
 
-  ami                         = "ami-03c7c1f17ee073747"
+  ami                         = "ami-09538990a0c4fe9be"
   instance_type               = "t2.micro"
-  availability_zone           = "us-west-2a"
+  availability_zone           = "us-east-1a"
   associate_public_ip_address = "true"
-  key_name                    = "A-M"
+  key_name                    = "CICD"
 }
